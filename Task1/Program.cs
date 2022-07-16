39bde0ed3f05311bfa097933c19a61189fd6e791
sTorov <sotorov2011@yandex.ru>
2022-07-16 20:42:29 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task1
+{
+    class OneOrTwoException : Exception
+    {
+        public OneOrTwoException(string message) : base(message)  { }
+    }
+    
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF7;
+            
+            List<Exception> exceptions = new List<Exception>();
+
+            exceptions.Add(new FileNotFoundException("Файл не найден!"));
+            exceptions.Add(new StackOverflowException("Стек выполнения привысил размер стека!"));
+            exceptions.Add(new DriveNotFoundException("Диск не доступен либо отсутствует!"));
+            exceptions.Add(new IndexOutOfRangeException("Индекс находится за пределами границ массива или коллекции!"));
+            exceptions.Add(new OneOrTwoException("Введённое значение должно быть 1 или 2!"));
+
+            for (int i = 0; i < exceptions.Count; i++)
+            {
+                try
+                {
+                    throw exceptions[i];
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.GetType().Name + ":", -30} {ex.Message}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
