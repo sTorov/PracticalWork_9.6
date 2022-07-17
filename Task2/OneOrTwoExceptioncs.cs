2e5cbeef04f39dda0d9da08c9e108f2588fdd765
sTorov <sotorov2011@yandex.ru>
2022-07-18 01:04:52 +0300

    Task2 Доработка OneOrTwoException.cs
    Добавлены описания для класса

--- Task2/OneOrTwoExceptioncs.cs
+++ Task2/OneOrTwoExceptioncs.cs
@@ -1,13 +1,23 @@
 ﻿namespace Task2
 {
+    /// <summary>
+    /// Исключение для обработки ситуаций, когда полученное значение отлично от 1 или 2
+    /// </summary>
     class OneOrTwoException : Exception
     {
+        /// <summary>
+        /// Хранит полученное значение, вызвавшее исключение
+        /// </summary>
         public string Value { get; }
         public OneOrTwoException(string message, string value) : base(message)
         {
             Value = value;
         }
 
+        /// <summary>
+        /// Вывод сообщение исключения OneOrTwoException на консоль с применением форматирования<br/>
+        /// Также выводит на консоль значение, вызвавшее исключение
+        /// </summary>
         public void PrintOneOrTwoException()
         {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
